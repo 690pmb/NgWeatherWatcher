09561eb97b41fd2ec92f8cf1c5d9f971db4c4fb5
69pmb <pmbroca@gmail.com>
2021-03-18 12:43:40 +0100

    refactor: fix eslint errors

--- src/app/shared/service/utils.service.ts
+++ src/app/shared/service/utils.service.ts
@@ -1,12 +1,12 @@
 import {
     HttpClient,
-    HttpHeaders,
     HttpErrorResponse,
+    HttpHeaders,
     HttpParams
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Observer, of } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { ToastService } from './toast.service';
 
 @Injectable({
@@ -30,13 +30,10 @@ export class UtilsService {
     public static findUserPosition(observer: Observer<string>): void {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
-                position => {
+                position =>
                     observer.next(
-                        position.coords.latitude +
-                            ',' +
-                            position.coords.longitude
-                    );
-                },
+                        `${position.coords.latitude},${position.coords.longitude}`
+                    ),
                 err => {
                     if (
                         err.code === GeolocationPositionError.PERMISSION_DENIED
@@ -53,16 +50,13 @@ export class UtilsService {
         }
     }
 
-    protected static getErrorMessage(error: any): string {
-        let message;
-        if (error.response) {
-            message = error.response.error;
-        } else if (error.error && error.error.errors) {
-            message =
-                'Status ' + error.status + ': ' + error.error.errors.join(', ');
-        } else if (error.error) {
-            message = error.error;
-        } else if (error.message) {
+    protected static getErrorMessage(
+        error: HttpErrorResponse | string | ErrorEvent
+    ): string {
+        let message: string;
+        if (error instanceof HttpErrorResponse) {
+            message = `Status ${error.status}: ${error.message}`;
+        } else if (error instanceof ErrorEvent) {
             message = error.message;
         } else {
             message = error;
@@ -72,12 +66,12 @@ export class UtilsService {
 
     static getHeaders(): HttpHeaders {
         return new HttpHeaders({
-            'Content-Type': 'application/json',
-            Authorization: UtilsService.bearer + localStorage.getItem('token')
+            'content-Type': 'application/json',
+            authorization: UtilsService.bearer + localStorage.getItem('token')
         });
     }
 
-    public handleError(error: any): void {
+    public handleError(error: HttpErrorResponse | string | ErrorEvent): void {
         console.error('handleError', error);
         this.toast.error(UtilsService.getErrorMessage(error));
     }
