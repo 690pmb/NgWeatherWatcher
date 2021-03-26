225b2b3c96d7fcd0ef9e3cd7777a2da65ecb292b
69pmb <pmbroca@gmail.com>
2021-03-26 16:07:37 +0100

    feat: forecast hour details

--- src/app/application-modules/dashboard/dashboard.module.ts
+++ src/app/application-modules/dashboard/dashboard.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
+import { DashboardDetailsComponent } from './components/dashboard-details/dashboard-details.component';
 import { DashboardForecastComponent } from './components/dashboard-forecast/dashboard-forecast.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
@@ -10,16 +12,25 @@ const childRoutes: Routes = [
     {
         path: '',
         component: DashboardForecastComponent
+    },
+    {
+        path: 'details/:date',
+        component: DashboardDetailsComponent
     }
 ];
 
 @NgModule({
-    declarations: [DashboardComponent, DashboardForecastComponent],
+    declarations: [
+        DashboardComponent,
+        DashboardDetailsComponent,
+        DashboardForecastComponent
+    ],
     imports: [
         MatProgressSpinnerModule,
+        MatTableModule,
         MatDividerModule,
         SharedModule.forChild(),
         RouterModule.forChild(childRoutes)
     ]
 })
-export class DashboardModule { }
+export class DashboardModule {}
