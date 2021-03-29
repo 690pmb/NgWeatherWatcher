76e839eebdf1ff2ce0ad9f839462867306ed2674
69pmb <pmbroca@gmail.com>
2021-03-29 11:46:22 +0200

    feat: icon pipe for condition

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,26 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from './service/auth.service';
-import { FormsModule } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
 import { HttpClientModule } from '@angular/common/http';
-import { MatButtonModule } from '@angular/material/button';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatListModule } from '@angular/material/list';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { TranslateModule } from '@ngx-translate/core';
+import { MenuComponent } from './component/menu/menu.component';
 import { SearchLocationComponent } from './component/search-location/search-location.component';
+import { IconPipe } from './pipe/icon.pipe';
+import { AuthService } from './service/auth.service';
 import { ToastService } from './service/toast.service';
 import { WeatherService } from './service/weather.service';
-import { MenuComponent } from './component/menu/menu.component';
 
 @NgModule({
-    declarations: [SearchLocationComponent, MenuComponent],
+    declarations: [SearchLocationComponent, MenuComponent, IconPipe],
     imports: [
         CommonModule,
         FormsModule,
@@ -41,6 +41,7 @@ import { MenuComponent } from './component/menu/menu.component';
         CommonModule,
         FormsModule,
         MatButtonModule,
+        IconPipe,
         MatSnackBarModule,
         SearchLocationComponent,
         MenuComponent
@@ -60,5 +61,5 @@ export class SharedModule {
 }
 
 export { AuthService } from './service/auth.service';
-export { WeatherService } from './service/weather.service';
 export { ToastService } from './service/toast.service';
+export { WeatherService } from './service/weather.service';
