bc8f3468b544c507739a0fe9b22f5b457257bd88
69pmb <pmbroca@gmail.com>
2022-07-23 17:31:45 +0200

    feat: select & delete alert (#115)

---
+++ src/app/application-modules/alert/click-outside.directive.ts
@@ -0,0 +1,25 @@
+import {Directive, HostListener, EventEmitter, Output} from '@angular/core';
+
+@Directive({
+  selector: '[appClickOutside]',
+})
+export class ClickOutsideDirective {
+  inside = false;
+  @HostListener('click')
+  clicked(): void {
+    this.inside = true;
+  }
+
+  @HostListener('document:click')
+  clickedOut(): void {
+    if (!this.inside) {
+      this.outside.emit(true);
+    }
+    this.inside = false;
+  }
+
+  @Output()
+  outside = new EventEmitter<boolean>();
+
+  constructor() {}
+}
