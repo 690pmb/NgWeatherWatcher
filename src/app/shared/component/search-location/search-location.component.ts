7f27191e7bf59a271927a6c27ac76024bee68230
69pmb <pmbroca@gmail.com>
2022-09-18 15:23:13 +0200

    fix: remove search btn on location input (#118)

--- src/app/shared/component/search-location/search-location.component.ts
+++ src/app/shared/component/search-location/search-location.component.ts
@@ -1,16 +1,32 @@
-import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+  OnInit,
+} from '@angular/core';
+import {FormControl} from '@angular/forms';
 import {MatAutocompleteTrigger} from '@angular/material/autocomplete';
 import {faLocationArrow, faTimes} from '@fortawesome/free-solid-svg-icons';
-import {Observable, Observer} from 'rxjs';
+import {iif, Observable, Observer, of} from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  catchError,
+  mergeMap,
+  switchMap,
+} from 'rxjs/operators';
 import {Location} from '../../../model/weather/location';
 import {WeatherService} from '../../../service/weather.service';
+import {Utils} from '../../utils';
 
 @Component({
   selector: 'app-search-location',
   templateUrl: './search-location.component.html',
   styleUrls: ['./search-location.component.scss'],
 })
-export class SearchLocationComponent {
+export class SearchLocationComponent implements OnInit {
   @Input()
   placeholder!: string;
 
@@ -20,29 +36,42 @@ export class SearchLocationComponent {
   @ViewChild(MatAutocompleteTrigger)
   trigger!: MatAutocompleteTrigger;
 
+  inputCtrl = new FormControl('');
   locations: Location[] = [];
   faLocationArrow = faLocationArrow;
   faTimes = faTimes;
 
   constructor(private weatherService: WeatherService) {}
 
-  search(term: string): void {
-    this.weatherService.search(term).subscribe(locations => {
-      this.locations = locations;
-      this.trigger.openPanel();
-    });
-  }
-
-  select(location: string): void {
-    this.selected.emit(location);
+  ngOnInit(): void {
+    this.inputCtrl.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        mergeMap(term =>
+          iif(
+            () => Utils.isNotBlank(term),
+            this.weatherService.search(term ?? ''),
+            of([])
+          )
+        ),
+        catchError(error => {
+          this.weatherService.handleError(error);
+          this.trigger.closePanel();
+          return of([]);
+        })
+      )
+      .subscribe(locations => (this.locations = locations));
   }
 
   geolocation(): void {
     new Observable((observer: Observer<string>) =>
       WeatherService.findUserPosition(observer)
-    ).subscribe(
-      location => this.search(location),
-      err => this.weatherService.handleError(err)
-    );
+    )
+      .pipe(switchMap(location => this.weatherService.search(location)))
+      .subscribe(locations => {
+        this.locations = locations;
+        this.trigger.openPanel();
+      });
   }
 }
