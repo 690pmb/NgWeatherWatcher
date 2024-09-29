be4cd496eea62c4a9258aebbb722883fb30b71bd
69pmb <pmbroca@gmail.com>
2024-09-29 20:01:33 +0200

    feat: wind direction

--- src/app/model/weather/hour.ts
+++ src/app/model/weather/hour.ts
@@ -1,12 +1,14 @@
 import {Condition} from './condition';
 
+export type WindDirection = 'E' | 'N' | 'NE' | 'NW' | 'S' | 'SE' | 'SW' | 'W';
+
 export type Hour = {
   time: string;
   tempC: number;
   isDay: boolean;
   condition: Condition;
   windKph: number;
-  windDir: string;
+  windDir: WindDirection;
   pressureMb: number;
   precipMm: number;
   humidity: number;
